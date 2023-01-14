34521ea23f9bb52d584a8e64d34f4bdf526be2c1
Pritish <pritish.s@affinidi.com>
2023-01-14 18:02:10 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Button,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import RTNCalculator from 'rtn-calculator/js/NativeCalculator.js';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [result, setResult] = useState<number | null>(null);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Button
+            title="Compute"
+            onPress={async () => {
+              const value = await RTNCalculator?.add(3, 7);
+              setResult(value ?? 0);
+            }}
+          />
+          <Text>{result}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
